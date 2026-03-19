c5b1fd52842347c899a520df92d406f1b45e5a1d
ennnui <mblokhina@sva.edu>
2026-03-19 14:44:04 -0400

    teeth animation draft + character motion fixed
    Thanks John!!!!!!!!!!!!!!!!!!!!!!!!!!!!!

---
+++ dentl-clinik/teeth.gd
@@ -0,0 +1,29 @@
+extends Node2D
+
+@export var mouth_closed = false
+var proximity_player = false
+var player_alive = true
+var min = 7
+var max = 15
+
+func _ready():
+	$AnimationPlayer/Timer.wait_time = randf_range(min,max)
+	$AnimationPlayer/Timer.start()
+
+#body enter
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	proximity_player = true
+
+func _process(delta: float) -> void:
+	if proximity_player and mouth_closed and player_alive:
+		#edit below for kill func l8r
+		print("player_is_dead") 
+		player_alive = false
+
+func _on_timer_timeout() -> void:
+	$AnimationPlayer.play("open_close")
+
+#body exit
+func _on_area_2d_body_exited(body: Node2D) -> void:
+	proximity_player = false
+	
