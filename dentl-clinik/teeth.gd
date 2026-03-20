d54a72f606685e967a9aa2e6b073e9e5ff3b1f6f
ennnui <mblokhina@sva.edu>
2026-03-19 23:17:49 -0400

    Minor graphics fixes

--- dentl-clinik/teeth.gd
+++ dentl-clinik/teeth.gd
@@ -3,17 +3,19 @@ extends Node2D
 @export var mouth_closed = false
 var proximity_player = false
 var player_alive = true
-var min = 7
-var max = 15
+var minimum = 7
+var maximum = 15
 
+#teeth animation start, random wait time range(?)
 func _ready():
-	$AnimationPlayer/Timer.wait_time = randf_range(min,max)
+	$AnimationPlayer/Timer.wait_time = randf_range(minimum,maximum)
 	$AnimationPlayer/Timer.start()
 
 #body enter
-func _on_area_2d_body_entered(body: Node2D) -> void:
+func _on_area_2d_body_entered(Node2D) -> void:
 	proximity_player = true
 
+#
 func _process(delta: float) -> void:
 	if proximity_player and mouth_closed and player_alive:
 		#edit below for kill func l8r
