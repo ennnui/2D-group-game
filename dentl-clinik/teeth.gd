e8e48584a9e980ee34b6ea75c941551b14c12fe6
ohwellelle <eleung2@sva.edu>
2026-03-21 12:38:24 -0400

    made the mouth interactable via staticbody2d

--- dentl-clinik/teeth.gd
+++ dentl-clinik/teeth.gd
@@ -15,12 +15,13 @@ func _ready():
 func _on_area_2d_body_entered(Node2D) -> void:
 	proximity_player = true
 
-#
+
 func _process(delta: float) -> void:
 	if proximity_player and mouth_closed and player_alive:
 		#edit below for kill func l8r
 		print("player_is_dead") 
 		player_alive = false
+	print(mouth_closed)
 
 func _on_timer_timeout() -> void:
 	$AnimationPlayer.play("open_close")
