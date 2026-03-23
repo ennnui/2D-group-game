838362566df1abc011f074299fbefc307546ab98
ohwellelle <eleung2@sva.edu>
2026-03-23 01:31:33 -0400

    fixed teeth killing and linked to death

--- dentl-clinik/teeth.gd
+++ dentl-clinik/teeth.gd
@@ -23,7 +23,7 @@ func _process(delta: float) -> void:
 	print(proximity_player)
 	
 	if proximity_player and player_alive:
-		#get_tree().reload_current_scene()
+		get_tree().change_scene_to_file("res://die.tscn")
 		print("player_is_dead") 
 		player_alive = false
 
